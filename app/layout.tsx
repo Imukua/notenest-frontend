17b504f621d4661103f060704b98956d80a7d96b
Imukua <ianmukuaa@gmail.com>
2024-10-19 01:55:03 +0300

    Refactor: Clean up layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+  import './globals.css'
+  import type { Metadata } from 'next'
+  import { Inter } from 'next/font/google'
+import { AuthProvider } from './context/AuthContext'
 
-const inter = Inter({ subsets: ['latin'] })
+  const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'NoteNest',
-  description: 'Capture your thoughts, unleash your potential',
-}
+  export const metadata: Metadata = {
+    title: 'NoteNest',
+    description: 'Capture your thoughts, unleash your potential',
+  }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
-}
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    )
+  }
