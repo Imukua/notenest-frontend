adf66da45263dc28b22cb7df816c491ae1f70ed7
Imukua <ianmukuaa@gmail.com>
2024-10-18 17:12:27 +0300

    Refractor: made footer a component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input"
 import { BookOpen, Edit3, Lock, Star, TrendingUp } from "lucide-react"
 import { Header } from "@/components/header/header"
 import { useRouter } from "next/navigation"
+import Footer from "@/components/footer/footer"
 
 export default function HomePage() {
   const router = useRouter()
@@ -112,17 +113,7 @@ export default function HomePage() {
           </div>
         </section>
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-slate-800">
-        <p className="text-xs text-slate-400">© 2024 JournalApp. All rights reserved.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4 text-slate-400 hover:text-blue-400" href="#">
-            Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4 text-slate-400 hover:text-blue-400" href="#">
-            Privacy
-          </Link>
-        </nav>
-      </footer>
+<Footer/>
     </div>
   )
 }
