16feb44c64b65f42ef6d25981b278e02c6056bb5
Imukua <ianmukuaa@gmail.com>
2024-10-18 21:14:18 +0300

    Refractor: sign up page route added

--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,6 +1,9 @@
+import { sign } from "crypto";
+
 export class Routes {
     public static readonly auth = {
         login: '/auth/login',
+        signup: '/auth/register',
         refreshTokens: '/auth/refresh',
         logout: '/auth/logout',
         me: '/auth/me',
