5fc845d28549d51fab482c490d1bda6acdcb3527
Imukua <ianmukuaa@gmail.com>
2024-10-25 20:46:28 +0300

    Refactor: Update font families in layout and tailwind.config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
-  import './globals.css'
+  import { Variable } from 'lucide-react'
+import './globals.css'
   import type { Metadata } from 'next'
   import { Inter } from 'next/font/google'
+  import { Roboto } from 'next/font/google'
 
-  const inter = Inter({ subsets: ['latin'] })
-
+  const roboto = Roboto({
+    subsets: ['latin'],
+    weight: ['400', '700'], // Select the font weights you need
+    variable: '--font-roboto', // Define a CSS variable for Tailwind usage
+  });
+  const inter = Inter({ 
+    subsets: ['latin'],
+    weight: ['400'], // Select the font weights you need
+    variable: '--font-inter', // Define a CSS variable for Tailwind usage
+  });
   export const metadata: Metadata = {
     title: 'NoteNest',
     description: 'Capture your thoughts, unleash your potential',
